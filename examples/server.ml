5af2d87e38722cb4982b40e9a052d6a11bd80657
zach <zachshipko@gmail.com>
2022-01-28 13:13:11 -0800

    Add output to server example

--- examples/server.ml
+++ examples/server.ml
@@ -6,6 +6,7 @@ let main =
   let config = Irmin_mem.config () in
   let uri = Uri.of_string "tcp://localhost:9090" in
   let* server = Server.v ~uri config in
+  let () = Format.printf "Listening on %a@." Uri.pp uri in
   Server.serve server
 
 let () = Lwt_main.run main
