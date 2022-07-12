a3c0d3550585d37aa85e2bff0a0b5710ad7f2c00
Patrick Ferris <patrick@sirref.org>
2022-07-12 10:31:22 +0100

    Sync remote store into local store

--- examples/sync.ml
+++ examples/sync.ml
@@ -1,24 +1,30 @@
 open Lwt.Syntax
 module Store = Irmin_mem.KV.Make (Irmin.Contents.String)
 module Client = Irmin_client_unix.Make (Store)
-module Sync = Irmin.Sync.Make (Client)
+module Sync = Irmin.Sync.Make (Store)
 
 let main =
   let uri = Utils.get_url in
   let* client = Client.connect uri in
   let* main = Client.main client in
+  let* repo = Store.Repo.v (Irmin_mem.config ()) in
+  let* local_main = Store.main repo in
   let* res = Client.ping client in
   match res with
   | Ok () -> (
       let* status =
-        Sync.pull_exn main (Irmin.Sync.remote_store (module Client) main) `Set
+        Sync.pull_exn local_main
+          (Irmin.Sync.remote_store (module Client) main)
+          `Set
       in
       match status with
       | `Empty ->
           Fmt.pr "Synced returned empty";
           Lwt.return_unit
       | `Head commit ->
-          Fmt.pr "Synced: %a" Client.Commit.pp_hash commit;
+          Fmt.pr "Synced: %a\n" Store.Commit.pp_hash commit;
+          let* lst = Store.list local_main [] in
+          Fmt.pr "Keys: %a\n" Fmt.(list string) (List.map fst lst);
           Lwt.return_unit)
   | Error e ->
       Fmt.pr "ERROR: %s\n" (Irmin_client.Error.to_string e);
